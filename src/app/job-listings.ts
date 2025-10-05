9459a86ab18a5425fd4595c0d430147e2908bdb2
Henry Moss <hdamoss@gmail.com>
2025-10-05 14:32:14 -0400

    Updated job listings and questions documents

--- src/app/job-listings.ts
+++ src/app/job-listings.ts
@@ -22,4 +22,25 @@ export const LISTINGS: JobListing[]  = [
       salary: 105000,
       flavorText: "Fly free standby while getting underpaid to work on call center dashboa..." 
     },
+    {
+      id: 4,
+      company: "North-by-Northwest Airlines",
+      role: "Precurement Officer",
+      salary: 250000,
+      flavorText: "Travel the world, don't ask questions, no one gets hurt..." 
+    },
+    {
+      id: 5,
+      company: "AskAlfred.com",
+      role: "Butler",
+      salary: 950000,
+      flavorText: "Looking for someone with professional experience in buttling..." 
+    },
+    {
+      id: 6,
+      company: "MugJudge",
+      role: "Front-End Developer",
+      salary: 75000,
+      flavorText: "Have you ever judged someone on their physical appearance? Do you want to?" 
+    },
   ]
