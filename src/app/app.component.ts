d9dcfc5a6c705588288e81f03706affdca00c648
shahzafrani <shahzafrani@gmail.com>
2025-10-07 17:23:17 -0400

    wrapper component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ScoreWrapperComponent } from './components/score-wrapper/score-wrapper.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ScoreWrapperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
